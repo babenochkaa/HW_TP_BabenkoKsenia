b42e29a0d897e95c6f0e108e86bf15cb8fb91aeb
Бабенко Ксения <kseniabb006@gmail.com>
2026-05-31 00:27:16 +0300

    Реализован класс Recipe

--- recipes.py
+++ recipes.py
@@ -15,3 +15,41 @@ class Ingredient:
             and self.name == other.name
             and self.unit == other.unit
         )
+
+class Recipe:
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        if ingredients is None:
+            self.ingredients = []
+        else:
+            self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for item in self.ingredients:
+            if item == ingredient:
+                item.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    def scale(self, ratio):
+        if ratio <= 0:
+            raise ValueError("Коэффициент должен быть больше 0")
+        new_ingredients = []
+        for ingredient in self.ingredients:
+            new_ingredients.append(
+                Ingredient(
+                    ingredient.name,
+                    ingredient.quantity * ratio,
+                    ingredient.unit
+                )
+            )
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        result = self.title + "\n"
+        for ingredient in self.ingredients:
+            result += str(ingredient) + "\n"
+        return result
