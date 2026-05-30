3c8d92ad894dd1c6838c73b1f03f5409a5e25343
Бабенко Ксения <kseniabb006@gmail.com>
2026-05-31 00:34:55 +0300

    Реализован класс DietaryRepice

--- recipes.py
+++ recipes.py
@@ -53,3 +53,15 @@ class Recipe:
         for ingredient in self.ingredients:
             result += str(ingredient) + "\n"
         return result
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio):
+        new_recipe = super().scale(ratio)
+        return DietaryRecipe(self.title, self.diet_type, new_recipe.ingredients)
+
+    def __str__(self):
+        return f"{self.title} ({self.diet_type})"
